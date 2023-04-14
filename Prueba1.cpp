42cfead7225eae42377732f6a94cbe32a6c5ad50
Diego Benjamín García Alfaro <00088023@uca.edu.sv>
2023-04-13 20:22:25 -0600

    :memo: Primer programa en C++ 13/4/23

---
+++ Prueba1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+const int currentYear = 2023;
+int anio, edad;
+int main()
+{
+    std::cout<<"Please insert your birth year: ";
+    std::cin >> anio;
+    edad = currentYear - anio;
+    std::cout<<"You are "<<edad<<" years old";
+    return 0;
+}
